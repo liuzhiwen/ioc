a1bb670d914f5e11d5e6e88b5a26d074aaa40d3f
liuzhiwen <liuzhiwen@up-tech.cn>
2022-04-09 22:08:12 +0800

    test

--- src/main/java/my/demo/ioc/StartApp.java
+++ src/main/java/my/demo/ioc/StartApp.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class StartApp {
     public static void main(String[] args) {
+        System.out.println("hello");
         SpringApplication.run(StartApp.class,args);
     }
 }
